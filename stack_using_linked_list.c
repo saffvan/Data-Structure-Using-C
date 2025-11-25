f43900f38a7999a1537f532dc74161c9914e6c13
Mohammed safvan. N <safvanmohammedn@gmail.com>
2025-11-25 01:50:17 -0500

    Add files via upload

---
+++ stack_using_linked_list.c
@@ -0,0 +1,22 @@
+// STACK USING LINKED LIST 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 5
+int counter = 0;
+
+struct stack
+{
+	int data;
+	struct stack *link;
+};
+struct stack *TOP = NULL;
+
+
+
+
+int main()
+{
+
+}
